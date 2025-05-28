f78664ceded875eb82274c0af51723913dfa671a
Rowonz <116256426+Rowonz@users.noreply.github.com>
2025-05-28 15:52:49 +0800

    Add files via upload

---
+++ User_inputs.py
@@ -0,0 +1,15 @@
+import logic_core as main
+inputs = None
+
+
+def inputs_main(iteration):
+    global inputs
+    inputs = {"acq_cost": float(input("Acquisition Cost: ").replace(',', '')),
+              "face_val": float(input("Face Value: ").replace(',', '')),
+              "inst_amt": float(input("Installment Amount: ").replace(',', '')),
+              "nom_rate": float(input("Nominal Rate (%): ").replace('%', '')) / 100}
+    inputs["years"] = round(inputs["face_val"] / inputs["inst_amt"])
+    difference = inputs["acq_cost"] - inputs["face_val"]
+    prm_disc = difference >= 0
+    amort_details_data = {"prm_disc": prm_disc, "diff": difference}
+    main.main_looper(inputs, amort_details_data, iteration)
